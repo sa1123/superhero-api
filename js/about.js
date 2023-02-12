864a044c809474d4699d29efa7ae3dd01973762d
Sahil <51375919+sa1123@users.noreply.github.com>
2023-02-13 01:27:37 +0530

    superhero

---
+++ js/about.js
@@ -0,0 +1,44 @@
+var resultId = localStorage.getItem('id');
+
+fetch();
+// getting all the data of the superhero
+function fetch(){
+    var xhrRequest = new XMLHttpRequest();
+
+    var url = `https://gateway.marvel.com/v1/public/characters/${resultId}?apikey=ee77679846ae512c45cd75dad1704dfb&hash=1fe14f8427c9003551a27df276a5daed&ts=1`;
+
+    xhrRequest.open('get', url, true);
+    xhrRequest.send();
+    xhrRequest.onload = function(){
+        var response = JSON.parse(xhrRequest.response);
+        document.getElementById('title').innerText = " " + response.data.results[0].name;
+        var power = document.getElementById('power');
+        var img = document.createElement('img');
+        img.src = response.data.results[0].thumbnail.path + "." + response.data.results[0].thumbnail.extension;
+        power.appendChild(img);
+
+        linebreak = document.createElement("br");
+        power.appendChild(linebreak);
+
+        var p = document.createElement('p');
+        
+        if(response.data.results[0].description == " " || response.data.results[0].description == ""){
+            p.innerText = "Description not provided by MarvelAPI"
+        }else{
+            p.innerText = "Description : " + response.data.results[0].description;
+        }
+        power.appendChild(p);
+
+        linebreak = document.createElement("br");
+        power.appendChild(linebreak);
+
+        console.log(response.data);
+
+        var p = document.createElement('p');
+        p.innerText = "Number of Series Available : " + response.data.results[0].series.available;
+        power.appendChild(p);
+        var p = document.createElement('p');
+        p.innerText = "Number of Stories Available : " + response.data.results[0].stories.available;
+        power.appendChild(p);
+    }
+}
---
+++ js/favourites.js
@@ -0,0 +1,43 @@
+
+var template = document.getElementById("template");
+console.log(localStorage.length);
+
+// traversing on local storage for favourites
+for(let i=0;i<localStorage.length;i++)
+{
+    if(localStorage.key(i) == 'id'){
+        continue;
+    }
+    let temp = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    console.log(temp);
+    var card = template.content.cloneNode(true);
+
+    console.log(card);
+    
+    //from here we add all the data of favourite character
+    card.getElementById("name").innerHTML = 'Name : ' + temp.name;
+    card.getElementById("id").innerHTML = 'Id : ' + temp.id;
+    if(temp.squareImage){
+        card.getElementById("img").innerHTML = "<img src = " + temp.squareImage + ">";
+    }else if(temp.thumbnail){
+        card.getElementById("img").innerHTML = "<img src = " + temp.thumbnail.path + "." + temp.thumbnail.extension + ">";
+    }else{
+        card.getElementById("img").innerHTML = "Image not provided by MarvelAPI";
+    }
+    
+
+    card.getElementById("more-info").addEventListener('click',function(){
+        localStorage.setItem('id',temp.id);
+        window.location.assign('./about.html');
+    });
+    //if user click on remove icon
+    card.getElementById("fav").addEventListener('click',function (){
+        card.innerHTML = null;
+        localStorage.removeItem(localStorage.key(i));
+        window.location.assign('./favourites.html');
+        alert('Removed from Favourites!')
+
+    });
+// appending to the list
+    document.getElementById("superhero-list").appendChild(card);
+}
